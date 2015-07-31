c67b0087f0af4882e288038e0ef6accd2e7689cd
Yehor Konoval <ekonoval@gmail.com>
2015-07-31 09:39:54 +0300

    fos user install

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,5 +1,6 @@
 <?php
 
+use FOS\UserBundle\FOSUserBundle;
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
@@ -16,6 +17,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            //new FOSUserBundle(),
             new AppBundle\AppBundle(),
         );
 
