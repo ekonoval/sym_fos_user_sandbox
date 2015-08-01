460cbe12f85e5d056591d99237c29dd73a283e88
Yehor Konoval <ekonoval@gmail.com>
2015-08-01 19:18:46 +0300

    clear, working and minimal configuration for fos User bundle

---
+++ src/AppBundle/Entity/EkvUser.php
@@ -0,0 +1,23 @@
+<?php
+namespace AppBundle\Entity;
+use FOS\UserBundle\Model\User;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class EkvUser extends User
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+}
