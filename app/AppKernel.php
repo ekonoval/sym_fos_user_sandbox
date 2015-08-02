be7d214b4962b87e377b34d7d96e9a00f7bf0aaa
Yehor Konoval <ekonoval@gmail.com>
2015-08-02 13:44:59 +0300

    "Overriding Default FOSUserBundle Controllers" - init. Create AppUserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOSUserBundle(),
             new AppBundle\AppBundle(),
+            new AppUserBundle\AppUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
