b3b8d3da10e41c1fa09311ad628535cc450e2658
Yehor Konoval <ekonoval@gmail.com>
2015-08-02 14:44:51 +0300

    trying writing logs

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,16 +3,33 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bridge\Monolog\Logger;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/app/example", name="homepage")
+     * @Route("/app/example", name="app_example")
+     */
+    public function appExampleAction()
+    {
+        return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/", name="homepage")
      */
     public function indexAction()
     {
-        return $this->render('default/index.html.twig');
+        echo "<h2>Vasya1   </h2>\n";
+
+        /** @var Logger $logger */
+        $logger = $this->container->get('ekv_logger');
+        $logger->info('fake');
+        $logger->warning('warn1');
+
+        return new Response('');
     }
 }
