1e6bf0e7a80aab77c6be652eb46141ad1178bbb1
Yehor Konoval <ekonoval@gmail.com>
2015-08-07 15:54:48 +0300

    install sonata-project/admin-bundle + doctrine orm admin bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,14 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOSUserBundle(),
+
+            // sonata
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
+
             new AppBundle\AppBundle(),
             new AppUserBundle\AppUserBundle(),
         );
